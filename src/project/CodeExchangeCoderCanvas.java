d9d9bd203e9e76870c9432030151f801e206f439
ElijahLyle <eminguez19@gmail.com>
2017-08-16 00:16:11 +0800

    Checking for real commit....

--- src/project/CodeExchangeCoderCanvas.java
+++ src/project/CodeExchangeCoderCanvas.java
@@ -3,12 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package project;
+package final_project;
+
+import javax.swing.JButton;
+
+import acm.graphics.*;
+import acm.program.ConsoleProgram;
+import acm.program.Program;
 
 /**
  *
- * @author Rivera
+ * @author Minguez	//Changed
  */
-public class CodeExchangeCoderCanvas {
-    
+public class CodeExchangeCoderCanvas extends ConsoleProgram{
+	
+	GCanvas canvas;
+	JButton checkClientRequests;
+	JButton checkPendingRequests;
+    public CodeExchangeCoderCanvas()
+    {
+    	//Test initialization for canvas of coders
+    	setSize(600,480);
+    	canvas = new GCanvas();
+    	checkClientRequests = new JButton("Check Client Requests");
+    	add(checkClientRequests,NORTH + getWidth()/2);
+    	checkPendingRequests = new JButton("Check Pending Requests");
+    	add(checkPendingRequests, NORTH + getWidth()/2 + 50);
+    }
+    public static void main(String[] args)
+    {
+    	new CodeExchangeCoderCanvas().start(args);
+    }
 }
