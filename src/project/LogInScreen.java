7ba7bfab7eb5c3cb3eb2f132386d8a1ba4a0677d
Rivera <Rivera@10.2.200.124>
2017-08-18 01:34:52 +0800

    created the login logic.

--- src/project/LogInScreen.java
+++ src/project/LogInScreen.java
@@ -26,6 +26,11 @@ public class LogInScreen extends javax.swing.JPanel {
         this.database = database;
     }
 
+    LogInScreen() {
+        
+    
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
